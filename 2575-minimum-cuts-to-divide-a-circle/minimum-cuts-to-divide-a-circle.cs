1e4e7311faaf92a065dffd87b5aea294a4c75ae9
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2024-03-07 18:43:16 +0100

    Time: 28 ms (5.88%) | Memory: 26.7 MB (58.82%) - LeetSync

---
+++ 2575-minimum-cuts-to-divide-a-circle/minimum-cuts-to-divide-a-circle.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int NumberOfCuts(int n) {
+        if(n % 2 == 0)
+            return n / 2;
+        else if(n == 1)
+            return 0;
+        return n;
+    }
+}
