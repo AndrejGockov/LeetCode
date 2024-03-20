f6530a2b5efe01f93333881b087fdff9867551bf
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2024-03-21 00:11:11 +0100

    Time: 4 ms (9.59%) | Memory: 7.6 MB (61.02%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+        vector<string> words;
+        string currentWord = "";
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != ' '){
+                currentWord += s[i];
+            }else{
+                // cout<<currentWord<<" ";
+                words.push_back(currentWord);
+                currentWord = "";
+            }
+        }
+        if(currentWord != "")
+            words.push_back(currentWord);
+
+        string sortedWords[words.size()];
+
+        for(int i = 0; i < words.size(); i++){
+            int position = 1;
+            for(char j : words[i]){
+               if(isdigit(j)){
+                    position = (int)j - (int)'0';
+                    cout<<position<<" ";
+                    sortedWords[position - 1] = words[i];
+                    continue;
+               }
+            }
+        }
+        
+        string ans = "";
+
+        for(int i = 0; i < words.size(); i++){
+            ans += sortedWords[i];
+            ans.erase(ans.size() - 1, 1);
+            if(i != words.size() - 1)
+                ans+= " ";
+        }
+        
+        return ans;
+    }
+};
