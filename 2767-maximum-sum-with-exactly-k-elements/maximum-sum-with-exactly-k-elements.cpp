a28c149f8f5711594f3e5a17547191662c017ac9
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2024-04-01 21:06:22 +0200

    Time: 37 ms (19.26%) | Memory: 75.3 MB (34.20%) - LeetSync

--- 2767-maximum-sum-with-exactly-k-elements/maximum-sum-with-exactly-k-elements.cpp
+++ 2767-maximum-sum-with-exactly-k-elements/maximum-sum-with-exactly-k-elements.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int maximizeSum(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
         int ans = 0;
+        sort(nums.begin(), nums.end());
 
         for(int i = 0; i < k; i++){
             ans += nums[nums.size() - 1];
