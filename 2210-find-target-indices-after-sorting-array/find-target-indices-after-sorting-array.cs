d62301c7ddb9b53c8fa0e309e182580471f80072
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2024-04-08 11:30:05 +0200

    Time: 105 ms (61.80%) | Memory: 46.5 MB (76.40%) - LeetSync

---
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public IList<int> TargetIndices(int[] nums, int target) {
+        IList<int> ans  = new List<int>();
+        Array.Sort(nums);
+        for(int i = 0; i < nums.Length; i++){
+            if(nums[i] == target){
+                ans.Add(i);
+            }
+        }
+        return ans;
+    }
+}
