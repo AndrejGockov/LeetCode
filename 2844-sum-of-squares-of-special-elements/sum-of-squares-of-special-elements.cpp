47812aac1e7f4625bf399dc213ad8550e0b904e9
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2024-06-29 01:01:52 +0200

    Time: 3 ms (96.34%) | Memory: 38.8 MB (34.18%) - LeetSync

--- 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.cpp
+++ 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.cpp
@@ -3,12 +3,9 @@ public:
     int sumOfSquares(vector<int>& nums) {
         int sum = 0;
         int n = nums.size();
-        for(int i = 0; i < nums.size(); i++){
-            if(n % (i + 1) == 0){
-                cout<<nums[i] << " ";
+        for(int i = 0; i < nums.size(); i++)
+            if(n % (i + 1) == 0)
                 sum += (nums[i] * nums[i]);
-            }
-        }
         return sum;
     }
 };
