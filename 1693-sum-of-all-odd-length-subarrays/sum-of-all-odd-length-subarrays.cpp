2256955d6c2c221cfed1aff89d9310639a0a1a91
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2024-06-29 01:16:17 +0200

    Time: 3 ms (52.04%) | Memory: 10.2 MB (13.78%) - LeetSync

--- 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
@@ -3,7 +3,6 @@ public:
     int sumOddLengthSubarrays(vector<int>& arr) {
         int ans = 0;
         for(int i = 0; i < arr.size(); i++){
-            int currSum = 0;
             for(int j = i; j < arr.size(); j+=2){
                 for(int k = i; k <= j; k++)
                     ans+=arr[k];
