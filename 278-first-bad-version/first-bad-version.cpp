9cb5e9bd396e280a3e37b44fa5b80964154e22c3
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2024-06-30 22:38:19 +0200

    Time: 1937 ms (5.02%) | Memory: 7 MB (99.31%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -4,9 +4,13 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
+        int i = 1;
         while(n > 0){
             if(!isBadVersion(n))
                 return n+1;
+            if(isBadVersion(i))
+                return i;
+            i++;
             n--;
         }
         return 1;
