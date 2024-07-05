26ef93a278d89ca58e4a6133c9e08eb5c4c4b576
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2024-07-05 09:41:46 +0200

    Time: 9 ms (55.94%) | Memory: 14 MB (99.61%) - LeetSync

--- 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.cpp
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.cpp
@@ -3,13 +3,12 @@ public:
     vector<int> intersection(vector<vector<int>>& nums) {
         vector<int>ans;
 
+        // Because it the number has to appear everywhere we only itterate through every element in the first sub-vector
         for(int i = 0; i < nums[0].size(); i++){
-            bool found = false;
+            bool found = true;
             for(int j = 0; j < nums.size(); j++){
-                // find(v.begin(), v.end(), x) != v.end(
-                if(find(nums[j].begin(), nums[j].end(), nums[0][i]) != nums[j].end()){
-                    found = true;
-                }else{
+                // Checks if the 
+                if(find(nums[j].begin(), nums[j].end(), nums[0][i]) == nums[j].end()){
                     found = false;
                     break;
                 }
