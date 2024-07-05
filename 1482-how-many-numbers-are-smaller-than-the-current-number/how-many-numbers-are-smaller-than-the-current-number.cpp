e296622f4de20573c9a4fe41b073baf842685e40
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2024-07-05 18:36:31 +0200

    Time: 7 ms (79.75%) | Memory: 12.6 MB (38.98%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -5,7 +5,7 @@ public:
         for(int i = 0; i < nums.size(); i++){
             int tmp = 0;
             for(int j = 0; j < nums.size(); j++){
-                if(j != i && nums[i] > nums[j])
+                if(nums[i] > nums[j])
                     tmp++;
             }
             ans.push_back(tmp);
