147a9fa8a1a3040d467f459918b12d95496e9796
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2024-07-05 10:36:22 +0200

    Time: 8 ms (54.37%) | Memory: 15.6 MB (31.23%) - LeetSync

--- 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.cpp
+++ 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     int countPrefixes(vector<string>& words, string s) {
         int ans = 0;
+
         for(int i = 0; i < words.size(); i++){
+            // If the current prefix is longer than the word it skips the current word
             if(words[i].size() > s.size())
                 continue;
+
+            // Checks every character in the current prefix
             bool found = true;
-            for(int j = 0; j < s.size(); j++){
-                if(j == words[i].size())
-                    break;
+            for(int j = 0; j < words[i].size(); j++){
                 if(words[i][j] != s[j]){
                     found = false;
                     break;
