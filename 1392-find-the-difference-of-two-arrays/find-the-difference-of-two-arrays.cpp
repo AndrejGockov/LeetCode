3533be5366feb56ed7fac0c37453a0a6be685d95
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2024-07-05 10:01:10 +0200

    Time: 71 ms (12.33%) | Memory: 29.2 MB (97.63%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -6,15 +6,15 @@ public:
         vector<vector<int>> ans;
 
         for(int i = 0; i < nums1.size(); i++){
-            if(find(nums2.begin(), nums2.end(),nums1[i]) == nums2.end())
-                if(find(vecOne.begin(), vecOne.end(), nums1[i]) == vecOne.end())
-                    vecOne.push_back(nums1[i]);
+            if(find(nums2.begin(), nums2.end(),nums1[i]) == nums2.end()
+            && find(vecOne.begin(), vecOne.end(), nums1[i]) == vecOne.end())
+                vecOne.push_back(nums1[i]);
         }
 
         for(int i = 0; i < nums2.size(); i++){
-            if(find(nums1.begin(), nums1.end(),nums2[i]) == nums1.end())
-                if(find(vecTwo.begin(), vecTwo.end(), nums2[i]) == vecTwo.end())
-                    vecTwo.push_back(nums2[i]);
+            if(find(nums1.begin(), nums1.end(),nums2[i]) == nums1.end()
+            && find(vecTwo.begin(), vecTwo.end(), nums2[i]) == vecTwo.end())
+                vecTwo.push_back(nums2[i]);
         }
 
         ans.push_back(vecOne);
