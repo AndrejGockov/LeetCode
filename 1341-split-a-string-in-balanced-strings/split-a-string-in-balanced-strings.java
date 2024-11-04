a641eb74c4a24e6550446f2c71c491d24b6eac13
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2024-11-04 18:24:07 +0100

    Time: 0 ms (100.00%) | Memory: 41.3 MB (36.27%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -3,11 +3,10 @@ class Solution {
         int L = 0;
         int ans = 0;
         for(char i : s.toCharArray()){
-            if(i == 'L'){
+            if(i == 'L')
                 L++;
-            }else{
+            else
                 L--;
-            }
 
             if(L == 0)
                 ans++;
