de599823a479f8220845306f28da4b68df8f595c
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2024-11-12 13:36:58 +0100

    Time: 37 ms (82.80%) | Memory: 45.2 MB (8.73%) - LeetSync

--- 2154-minimum-moves-to-convert-string/minimum-moves-to-convert-string.java
+++ 2154-minimum-moves-to-convert-string/minimum-moves-to-convert-string.java
@@ -4,10 +4,7 @@ class Solution {
 
         for(int i = 0; i < s.toCharArray().length; i++){
             if(s.toCharArray()[i] == 'X'){
-                // while(s.toCharArray()[i] == 'X' 
-                // && i < s.toCharArray().length - 2){
-                    i += 2;
-                // }
+                i += 2;
                 ans++;
             }
         }
