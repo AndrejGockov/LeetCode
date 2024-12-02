6773d06e9493a2932f3a3bb124f6ed330a414dc3
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2024-12-03 00:27:33 +0100

    Time: 5 ms (64.63%) | Memory: 44.8 MB (44.51%) - LeetSync

--- 2159-two-out-of-three/two-out-of-three.java
+++ 2159-two-out-of-three/two-out-of-three.java
@@ -1,16 +1,19 @@
 class Solution {
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
         List<Integer>ans = new ArrayList<Integer>(nums1.length + nums2.length + nums3.length);
+
         HashSet<Integer> hash = new HashSet<>();
         HashSet<Integer> hashTwo = new HashSet<>();
         HashSet<Integer> hashThree = new HashSet<>();
 
+        // Can't add anything yet, because it's only in 1 Array
         for(int i : nums1){
             if(!hash.contains(i)){
                 hash.add(i);
             }
         }
 
+        // Add to ans if the number isn't in ans and is in the first array
         for(int i : nums2){
             if(hash.contains(i) && !ans.contains(i)){
                 ans.add(i);
@@ -19,6 +22,8 @@ class Solution {
             }
         }
 
+        // Add to ans if the number isn't in ans 
+        // and is in either the first or second array
         for(int i : nums3){
             if((hash.contains(i) || hashTwo.contains(i))
             && !ans.contains(i)){
