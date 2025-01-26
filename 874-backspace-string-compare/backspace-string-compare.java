4f907985d344e9da7b47e8c9b7df740ab535266c
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2025-01-26 16:01:22 +0100

    Time: 9 ms (7.42%) | Memory: 42.9 MB (5.74%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String stackToString(Stack<Character>stack){
+        Iterator value = stack.iterator();
+        String res = "";
+
+        while(value.hasNext()){
+            res+=value.next();
+        }
+
+        return res;
+    }
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> stackOne = new Stack<>();
+        Stack<Character> stackTwo = new Stack<>();
+
+        for(char i : s.toCharArray()){
+            if(i == '#' && !stackOne.isEmpty())
+                stackOne.pop();
+            
+            if(i != '#')
+                stackOne.push(i);
+        }
+
+        for(char i : t.toCharArray()){
+            if(i == '#' && !stackTwo.isEmpty())
+                stackTwo.pop();
+            
+            if(i != '#')
+                stackTwo.push(i);
+        }
+        System.out.println(stackToString(stackOne) + " " + stackToString(stackTwo));
+        return stackToString(stackOne).equals(stackToString(stackTwo));
+    }
+}
