7dcd94697992302fcc5adc36cb9f4408769a5aad
Andrej Gockov <134962357+AndrejGockov@users.noreply.github.com>
2025-07-15 01:39:37 +0200

    Time: 68 ms (16.83%) | Memory: 51.7 MB (23.78%) - LeetSync

---
+++ 1582-design-browser-history/design-browser-history.java
@@ -0,0 +1,50 @@
+class BrowserHistory {
+    String homePage = "";
+    Stack<String>history = new Stack<>();
+    Stack<String>stepsBack = new Stack<>();
+
+    public BrowserHistory(String homepage) {
+        homePage = homepage;
+    }
+    
+    public void visit(String url) {
+        history.add(url);
+        stepsBack.clear();
+    }
+    
+    public String back(int steps) {
+        for(int i = 0; i < steps; i++){
+            if(history.isEmpty())
+                break;
+            
+            stepsBack.add(history.pop());
+        }
+
+        if(history.isEmpty())
+            return homePage;
+        
+        return history.peek();
+    }
+    
+    public String forward(int steps) {
+        for(int i = 0; i < steps; i++){
+            if(stepsBack.isEmpty())
+                break;
+            
+            history.add(stepsBack.pop());
+        }
+
+        if(history.isEmpty())
+            return homePage;
+
+        return history.peek();
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
