a3c39b8b51108f5e30c40313aaadddd29b036729
Andrej Gockov <gockovandrej@gmail.com>
2026-02-06 23:14:09 +0100

    Time: 805 ms (7.16%) | Memory: 0B (100.00%) - LeetSync

---
+++ 178-rank-scores/rank-scores.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select score, (
+    select count(distinct s2.score)
+    from scores s2
+    where s2.score >= s1.score
+) as 'rank'
+from scores s1
+order by score desc
